99c21ae4273040085a870a48db57ce2c89ed7113
Pedro Henrique <pedrohenriqueborgesodia@gmail.com>
2026-02-03 19:06:09 -0300

    Update sqlsmartdata.sql

--- SmartData/DataBase/sqlsmartdata.sql
+++ SmartData/DataBase/sqlsmartdata.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `clientes`
 --
 
+CREATE DATABASE IF NOT EXISTS `gerenciamento_clientes`;
+USE `gerenciamento_clientes`;
+
 DROP TABLE IF EXISTS `clientes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
